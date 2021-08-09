24e10613f5bac6499d81c8df8939ea856c8d1a13
Mike <mike@thinkitfirst.com>
2021-08-09 13:33:30 -0400

    Updated default Bootstrap version for Sage Installer

--- src/Presets/Bootstrap5.php
+++ src/Presets/Bootstrap5.php
@@ -7,8 +7,8 @@ class Bootstrap5 extends Preset
     /** {@inheritdoc} */
     protected function updatePackagesArray(array $packages)
     {
-        $packages['dependencies']['bootstrap'] = 'v5.0.1';
-        $packages['dependencies']['@popperjs/core'] = '^2.9.2';
+        $packages['dependencies']['bootstrap'] = 'v5.1.0';
+        $packages['dependencies']['@popperjs/core'] = '^2.9.3';
         return $packages;
     }
 }
